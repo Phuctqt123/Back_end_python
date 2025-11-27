fd6256028fa99497142024b5806f065d86df2acb
bkucse <phuc.buicogang@hcmut.edu.vn>
2025-11-27 20:39:29 +0700

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 import torch
 from sentence_transformers import SentenceTransformer, util
 from pyvi.ViTokenizer import tokenize
+import uvicorn
+import os
 
 # ================== KHỞI TẠO MODEL ==================
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -23,21 +25,21 @@ emb_majors = model.encode(major_sentences, convert_to_tensor=True, device=device
 
 # ================== API ==================
 app = FastAPI()
-
-
 class UserInput(BaseModel):
     text: str
 
+@app.get("/")
+def home():
+    return {"status": "ok"}
 
 @app.post("/predict")
 def predict_major(data: UserInput):
     user_tok = tokenize(data.text)
     emb_user = model.encode(user_tok, convert_to_tensor=True, device=device)
 
-    # Tính điểm giống nhau
+    # So sánh tương đồng
     scores = util.cos_sim(emb_user, emb_majors)[0]
 
-    # Tạo danh sách kết quả trả về
     results = []
     for (name, _), score in zip(majors.items(), scores):
         results.append({
@@ -51,3 +53,8 @@ def predict_major(data: UserInput):
         "best_major": best_major,
         "scores": results
     }
+
+# Chạy server
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
